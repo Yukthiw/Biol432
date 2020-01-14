6ad55eefd982a9133a84132dc5f942566337f105
Yukthiw <yukthiw61@gmail.com>
2020-01-14 00:03:24 -0500

    Added Part 1 of Assignment 1

---
+++ DataGeneration.R
@@ -0,0 +1,31 @@
+Species=c("Macaca mulatta","Mandrillus sphinx","Macaca silenus",
+          "Macaca fuscata","Nasalis larvatus","Papio papio",
+          "Saguinus imperator","Macaca radiata","Chlorocebus sabaeus",
+          "Papio anubis")
+
+Limb.Width=rnorm(n=10,mean=10,sd=10)
+Limb.Width=abs(Limb.Width)
+
+unitsW=rbinom(prob=0.5,n=10,size=1)
+for(i in 0:10){
+  if(i==1){
+    unitsW[i]="mm"
+  }else{
+    unitsW[i]="cm"
+  }
+}
+
+Limb.Length=rnorm(n=10,mean=10,sd=10)
+Limb.Length=abs(Limb.Length)
+
+unitsL=rbinom(prob=0.5,n=10,size=1)
+for(i in 0:10){
+  if(i==1){
+    unitsL[i]="mm"
+  }else{
+    unitsL[i]="cm"
+  }
+}
+
+MyData = data.frame(Species,Limb.Width,unitsW,Limb.Length,unitsL)
+MyData
